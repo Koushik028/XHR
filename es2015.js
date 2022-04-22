2a32aced37943c58c7ff85db03c7171fb79255d3
koushik P <103559026+Koushik028@users.noreply.github.com>
2022-04-22 22:05:08 +0530

    Add files via upload

---
+++ es2015.js
@@ -0,0 +1,14 @@
+const getCountries = () => {
+    const xmr=new XMLHttpRequest();
+    xmr.open("GET","https://restcountries.com/v3.1/all");
+    xmr.send();
+    xmr.responseType="json";
+     xmr.onload =() =>{
+        const countries=xmr.response;
+        for(let i in countries)
+        {
+            console.log(countries[i].name.common,countries[i].population);
+        }
+    };
+};
+getCountries();
